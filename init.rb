c53a294b5a431b1a0a63eee6a67a11a174bc28e7
Kevin Porras <kporras07@gmail.com>
2020-09-04 13:11:00 -0600

    Update authorship to EW

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
 Redmine::Plugin.register :redmine_timesheet_filter_autocomplete do
   name 'Redmine Timesheet Filter Autocomplete plugin'
-  author 'Kevin Porras'
+  author 'Evolving Web'
   description 'Autocomplete filter for Timesheets'
   version '0.0.1'
-  url 'https://github.com/kporras07/redmine_timesheet_filter_autocomplete'
-  author_url 'https://kporras07.com'
+  url 'https://github.com/evolvingweb/redmine_timesheet_filter_autocomplete'
+  author_url 'https://evolvingweb.ca'
 end
